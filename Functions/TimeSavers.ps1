1e307fb265a2bd6f09142d0e175d1f067a68e488
eames1127 <daniel.eames@googlemail.com>
2020-07-03 23:45:13 +0100

    Added new functions and linked to profile

--- Functions/TimeSavers.ps1
+++ Functions/TimeSavers.ps1
@@ -1,9 +1,30 @@
-function openFolder($location)
-{
-    Set-Location $location;
-}
+$codingLoc = "C:\Coding";
 
 function goToGitHubRepo($loc)
 {
-    https://github.com/eames1127/$loc;
+    $url = "https://github.com/eames1127/"+ $loc;
+    Start-Process "chrome.exe" $url;
+}
+
+function TestLink()
+{
+    Write-Host "Hello";
+}
+
+function goToCoding()
+{
+    Set-Location $codingLoc;
+    code .;
+}
+
+function initRepo($loc)
+{
+    Set-Location $codingLoc;
+    git clone $loc;
+}
+
+function goToRepo($repo){
+    $codingRepo = $codingLoc + "\" + $repo;
+    Set-Location $codingRepo;
+    git status;
 }
