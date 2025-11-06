32e16f2152fcc95efcf4832afbbc19ed30ad4c71
eames1127 <54189781+eames1127@users.noreply.github.com>
2025-11-06 21:03:48 +0000

    General code tidyup

--- Functions/TimeSavers.ps1
+++ Functions/TimeSavers.ps1
@@ -1,51 +1,47 @@
-$codingLoc = "C:\Coding";
-$username = "user";
-$psProfileLoc = "C:\Users\$username\Documents\WindowsPowerShell";
-function goToGitHubRepo($username, $loc)
-{
-    Write-Host "Heading to GitHub repo for" $loc;
-    $url = "https://github.com/" + $username + "/" + $loc;
-    Start-Process $url;
+$codingLoc = "C:\Coding"
+$username = "user"
+$psProfileLoc = "C:\Users\$username\Documents\WindowsPowerShell"
+function goToGitHubRepo($username, $loc) {
+    Write-Host "Heading to GitHub repo for $loc"
+    $url = "https://github.com/$username/$loc"
+    Start-Process $url
 }
 
-function goToCoding()
-{
-    Write-Host "Heading to " $codingLoc;
-    Set-Location $codingLoc;
-    code .;
-}
-
-function initRepo($loc)
-{
-    Write-Host "Heading to " $codingLoc;
-    Set-Location $codingLoc;
-    Write-Host "Cloning into " $loc
-    git clone $loc;
-}
-
-function goToRepo($repo){
-    $codingRepo = $codingLoc + "\" + $repo;
-    Set-Location $codingRepo;
-    git status;
-}
-
-function psProfile()
-{
-    Write-Host "Heading to "  $psProfileLoc;
-    Set-Location $psProfileLoc;
+function goToCoding() {
+    Write-Host "Heading to $codingLoc"
+    Set-Location $codingLoc
     code .
 }
 
-function copyPSProfileToCode(){
-    Write-Host "Copying from" $psProfileLoc;
-    Copy-Item -Path "$psProfileLoc\Microsoft.PowerShell_profile.ps1" -Destination "C:\Coding\PowerShellProfile" -Recurse;
-    Copy-Item -Path "$psProfileLoc\Functions" -Destination "C:\Coding\PowerShellProfile\Functions" -Recurse;
+function initRepo($loc) {
+    Write-Host "Heading to $codingLoc"
+    Set-Location $codingLoc
+    Write-Host "Cloning into $loc"
+    git clone $loc
 }
 
-function copyPSProfileFromCode(){
-    Write-Host "Copying from" $psProfileLoc;
-    Copy-Item -Path "$psProfileLoc\Microsoft.PowerShell_profile.ps1" -Destination "C:\Coding\PowerShellProfile" -Recurse;
-    Copy-Item -Path "$psProfileLoc\Functions" -Destination "C:\Coding\PowerShellProfile\Functions" -Recurse;
+function goToRepo($repo) {
+    $codingRepo = "$codingLoc\$repo"
+    Set-Location $codingRepo
+    git status
+}
+
+function psProfile() {
+    Write-Host "Heading to $psProfileLoc"
+    Set-Location $psProfileLoc
+    code .
+}
+
+function copyPSProfileToCode() {
+    Write-Host "Copying from $psProfileLoc"
+    Copy-Item -Path "$psProfileLoc\Microsoft.PowerShell_profile.ps1" -Destination "$codingLoc\PowerShellProfile" -Recurse
+    Copy-Item -Path "$psProfileLoc\Functions" -Destination "$codingLoc\PowerShellProfile\Functions" -Recurse
+}
+
+function copyPSProfileFromCode() {
+    Write-Host "Copying to $psProfileLoc"
+    Copy-Item -Path "$codingLoc\PowerShellProfile\Microsoft.PowerShell_profile.ps1" -Destination $psProfileLoc -Recurse
+    Copy-Item -Path "$codingLoc\PowerShellProfile\Functions" -Destination $psProfileLoc -Recurse
 }
 
 <#
@@ -58,8 +54,7 @@ Your Git checkout location. e.g. c:/coding/git
 .PARAMETER branchName
 Name of comparision branch
 #>
-function getChangesInBranch{
- 
+function getChangesInBranch {
     param(
         [Parameter(Mandatory=$true)]
         [String]$gitLoc,
@@ -71,17 +66,15 @@ function getChangesInBranch{
 
     Set-Location $gitLoc
 
-    if ($update -eq "y"){
+    if ($update -eq "y") {
         git pull
     }
     $lastCommitHash = git log -n 1 $branchName --pretty=format:"%H"
+    $branch = git branch --show-current
 
-    #must be using Git version 2.2 or above for this to work.
-    $branch = git branch --show-current 
-
-    Write-Host "`n Warning you are currently on $branch branch." -ForegroundColor Yellow
-    Write-Host "`nFetching all commits since $lastCommitHash. Ensure you press enter to view all commits until you see (END) then press q.`n" -ForegroundColor Green
-    git log $lastCommitHash..Head --oneline --merges|Where-Object {$_ -Like "*Merged in Jira*"} | Format-Table
+    Write-Host "`nWarning: you are currently on $branch branch." -ForegroundColor Yellow
+    Write-Host "`nFetching all commits since $lastCommitHash. Press enter to view all commits until you see (END) then press q.`n" -ForegroundColor Green
+    git log $lastCommitHash..Head --oneline --merges | Where-Object {$_ -Like "*Merged in Jira*"} | Format-Table
 
     Write-Host "`nScript Completed." -ForegroundColor Green
 }
